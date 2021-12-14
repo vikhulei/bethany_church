a480340d454106bf9b77dcf24fad6059df4c7be5
viktor <vikhulei@gmail.com>
2021-12-14 17:58:37 +0200

    local added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Bethany Church</h1>
+      <h1>Bethany Church Local</h1>
       <h2>Landing page</h2>
     </div>
   );
