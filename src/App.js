7ab8fd2b0fb7fcbfe5783cc5fb7cf80c1ebe3643
viktor <vikhulei@gmail.com>
2021-12-15 08:26:24 +0200

    assets added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./styles.css";
+import church_main from "./assets/church_main.jpg"
 
 export default function App() {
   return (
     <div className="App">
+      <div className="mainPicture">
+        <img src={church_main} alt="main picture" />
+      </div> 
       <h1>Bethany Church Local</h1>
       <h2>Landing page</h2>
     </div>
