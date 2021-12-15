4184fed76f0a213c3d2575363b1409c77de4df7f
vikhulei <65869915+vikhulei@users.noreply.github.com>
2021-12-15 08:31:41 +0200

    components and pages folders added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./styles.css";
-import church_main from "./assets/church_main.jpg"
+import church_main from "./assets/church_main.jpg";
 
 export default function App() {
   return (
     <div className="App">
       <div className="mainPicture">
         <img src={church_main} alt="main picture" />
-      </div> 
-      <h1>Bethany Church Local</h1>
+      </div>
+      <h1>Bethany Church</h1>
       <h2>Landing page</h2>
     </div>
   );
