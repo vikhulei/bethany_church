18d5bd5d8ba6ffd99202d2c420801ac64838db23
vikhulei <65869915+vikhulei@users.noreply.github.com>
2021-12-15 19:02:44 +0200

    changed alt in img

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="mainPicture">
-        <img src={church_main} alt="main picture" />
+        <img src={church_main} alt="church_main" />
       </div>
       <h1>Bethany Church</h1>
       <h2>Landing page</h2>
