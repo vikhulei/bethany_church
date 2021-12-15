da35221c8e8818ceb5ca178b4252e4c6d7aed550
vikhulei <65869915+vikhulei@users.noreply.github.com>
2021-12-15 19:25:23 +0200

    picture for background created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,28 @@
 import React from "react";
 import church_main from "../assets/church_main.jpg";
+import styled from "styled-components";
+
+const MainPicture = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const MainPictureImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
 
 const Home = () => {
   return (
     <>
-      <div className="mainPicture">
-        <img src={church_main} alt="church_main" />
-      </div>
+      <MainPicture>
+        <MainPictureImg src={church_main} alt="church_main" />
+      </MainPicture>
     </>
   );
 };
