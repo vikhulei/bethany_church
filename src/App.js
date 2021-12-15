eb33e78cc8f91c1b4fe9a45df68d786213903182
vikhulei <65869915+vikhulei@users.noreply.github.com>
2021-12-15 20:22:18 +0200

    App js cleaned

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./styles.css";
-// import church_main from "./assets/church_main.jpg";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
     <div className="App">
       <Home />
-      {/* <div className="mainPicture">
-        <img src={church_main} alt="church_main" />
-      </div>
-      <h1>Bethany Church</h1>
-      <h2>Landing page</h2> */}
     </div>
   );
 }
