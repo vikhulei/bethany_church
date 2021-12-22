91ba406d1c6de3f2bbfea9f84f16f099b0feefb8
vikhulei <65869915+vikhulei@users.noreply.github.com>
2021-12-22 08:11:03 +0200

    Home.js cleaned

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,92 +1,9 @@
 import React, { useState } from "react";
-import styled from "styled-components";
-import church_main from "../assets/church_main.jpg";
-import church_main_mobile from "../assets/church_main_mobile.jpg";
-import bethany from "../assets/bethany.png";
 import HomeMobile from "./mobile/HomeMobile";
 import HomeDesk from "./desktop/HomeDesk";
 
-const MainPicture = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -1;
-  width: 100vw;
-  height: 100vh;
-`;
-
-const MainPictureImg = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
-
-const Logo = styled.img`
-  position: absolute;
-  top: 5px;
-  left: 5px;
-  width: 100px;
-`;
-
-const MainText = styled.div`
-  position: absolute;
-  top: 20vh;
-  left: 10vw;
-`;
-
-const Welcome = styled.p`
-  font-family: "Rosarivo";
-  font-size: calc(4rem + 3vw);
-  color: white;
-  line-height: 1;
-`;
-
-const ComeAs = styled.p`
-  font-family: "Rosarivo";
-  font-size: calc(2rem + 2vw);
-  font-style: italic;
-  color: white;
-  margin-left: 100px;
-`;
-
-const BottomText = styled.div`
-  position: absolute;
-  bottom: 20px;
-  width: 100%;
-`;
-
-const NoNeed = styled.p`
-  font-family: "Rosarivo";
-  font-size: calc(1.5rem + 1vw);
-  color: white;
-  margin-left: 100px;
-  text-align: center;
-  text-shadow: black 0px 0px 50px;
-`;
-
 const Home = ({ mobileVersion }) => {
-  return (
-    <>
-      {mobileVersion ? <HomeMobile /> : <HomeDesk />}
-      {/* <MainPicture>
-        {mobileVersion ? (
-          <MainPictureImg src={church_main} alt="church_main" />
-        ) : (
-          <MainPictureImg src={church_main_mobile} alt="church_main" />
-        )}
-      </MainPicture>
-      <Logo src={bethany} />
-      <MainText>
-        <Welcome>Welcome</Welcome>
-        <ComeAs>come as you are</ComeAs>
-      </MainText>
-      <BottomText>
-        <NoNeed>
-          No need to get clever. Simply <br /> tell Him what you are up to
-        </NoNeed>
-      </BottomText> */}
-    </>
-  );
+  return <>{mobileVersion ? <HomeMobile /> : <HomeDesk />}</>;
 };
 
 export default Home;
