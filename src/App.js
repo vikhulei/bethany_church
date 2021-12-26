c527c2e2d649875d0da50c9ac57f859f0b8fc067
vikhulei <65869915+vikhulei@users.noreply.github.com>
2021-12-26 09:19:04 +0200

    wrapper removed ex b-color, pages max-width 100vw

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ import Services from "./pages/Services";
 import Contacts from "./pages/Contacts";
 
 const Wrapper = styled.div`
-  position: absolute;
+  /* position: absolute;
   width: 100vw;
-  height: 100vh;
-  display: flex;
+  height: 100%;
+  display: flex; */
   background-color: lightgray;
-  z-index: -99;
+  /* z-index: -99; */
 `;
 
 const Pages = styled.div`
-  width: 100vw;
+  max-width: 100vw;
 `;
 
 export default function App() {
