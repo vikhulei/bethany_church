329683f05fb2ab5d357af09dacb900aa54884506
vikhulei <65869915+vikhulei@users.noreply.github.com>
2021-12-28 22:38:35 +0200

    Language button designed

--- src/pages/Language.js
+++ src/pages/Language.js
@@ -5,20 +5,24 @@ const SelectLang = styled.div`
   position: fixed;
   right: 10px;
   top: 60px;
-  box-shadow: 10px 10px 30px black;
-  font-family: Arial, Helvetica, sans-serif;
-  font-weight: bold;
-  color: lightgray;
+  /* box-shadow: 10px 10px 30px black; */
+  /* font-family: Arial, Helvetica, sans-serif; */
+  font-weight: 600;
+  font-size: 0.9rem;
+  color: black;
   text-align: center;
-  /* background-color: grey; */
-  width: 40px;
+  /* border: solid 1px gray; */
+  padding: 3px;
+  background-color: rgb(180, 180, 180);
+  width: 65px;
   &:hover {
     cursor: pointer;
   }
+  z-index: 99;
 `;
 
 const Lang = () => {
-  return <SelectLang>UKR</SelectLang>;
+  return <SelectLang>укр/eng</SelectLang>;
 };
 
 export default Lang;
